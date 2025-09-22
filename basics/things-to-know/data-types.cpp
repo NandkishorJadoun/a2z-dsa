cadff9fbafda1ee29fa8baf9b39ef9d0a424405a
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-09-22 11:26:02 +0530

    learned about data types, array and strings

---
+++ basics/things-to-know/data-types.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string text = "hello";
+    cout << text;
+
+    string sentence;
+    getline(cin, sentence);
+    cout << sentence;
+    return 0;
+}
+
+// Datatypes to use:
+// int, long, long long, float, double
+// string and getline(for selecting the whole line in a string var)
+// char
