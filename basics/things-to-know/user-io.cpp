186df2b14be0ddb6026aa9c436a326fa1f12460d
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-09-22 08:56:21 +0530

    feat: practiced input and output in C++

---
+++ basics/things-to-know/user-io.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y;
+    return 0;
+}
