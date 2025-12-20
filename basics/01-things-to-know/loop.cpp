9491ce255485e9962f2077ed389fc4d897195cd3
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-12-20 08:11:58 +0530

    cpp basics

---
+++ basics/01-things-to-know/loop.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    /* for loop
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "run" << endl;
+    } */
+
+    /* while loop
+
+    int i = 0;
+    while (i < 10)
+    {
+        cout << "run" << endl;
+        i++;
+    } */
+
+    int i = 2;
+    do
+    {
+        cout << "run" << endl;
+        i++;
+    } while (i <= 1);
+
+    return 0;
+}
